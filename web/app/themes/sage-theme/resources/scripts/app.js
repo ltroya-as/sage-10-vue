d98aec315bec489ed8514bbf3a8646b12e31fdac
ltroya-as <luis.troya@allansolutions.com>
2023-03-16 21:58:29 -0500

    feat: project setup

---
+++ web/app/themes/sage-theme/resources/scripts/app.js
@@ -0,0 +1,20 @@
+import domReady from '@roots/sage/client/dom-ready';
+import App from './components/App.vue'
+import {createApp} from 'vue';
+import Vue from 'vue';
+
+/**
+ * Application entrypoint
+ */
+domReady(async () => {
+  // createApp({
+  //   // Every vue component has to be imported here in order to be available
+  //   // in every template.
+  //   components: {App}
+  // }).mount("#main");
+});
+
+/**
+ * @see {@link https://webpack.js.org/api/hot-module-replacement/}
+ */
+import.meta.webpackHot?.accept(console.error);
---
+++ web/app/themes/sage-theme/resources/scripts/components/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+  import { ref, onMounted } from 'vue'
+
+  const props = defineProps({
+    msg: {
+      type: String,
+    }
+  })
+
+  // reactive state
+  const count = ref(0)
+
+  // functions that mutate state and trigger updates
+  function increment() {
+    count.value++
+  }
+
+  // lifecycle hooks
+  onMounted(() => {
+    console.log(`The initial count is ${count.value}.`)
+  })
+</script>
+
+<template>
+  <h3>Here is the message prop: {{ props.msg }}</h3>
+  <button @click="increment" class="bg-blue-400 px-10 py-3">Count is: {{ count }}</button>
+</template>
